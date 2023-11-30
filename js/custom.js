6335c856d74ccf89e4d95258f5febd14c846f23e
freeman <iviewn@gmail.com>
2023-11-30 19:37:59 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -116,3 +116,25 @@ $('.modal__close').click(function() {
     $('.modal-wrap').removeClass('vis');
 });
 
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView : 2,
+    centeredSlides : true,
+    effect : 'coverflow',
+    coverflow: {
+              rotate: 0,
+              stretch: 0,
+              depth: 300,
+              modifier: 1,
+              // slideShadows : true
+          },
+     
+    // If we need pagination
+    // pagination: '.swiper-pagination',
+     
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+      
+    }) 
